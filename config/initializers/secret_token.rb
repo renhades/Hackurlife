c3c0bb100ace05282d60ecfa2c703bd41c02b1d3
ryan <ryan@laptop.(none)>
2014-09-14 18:35:00 +0800

    add initialized files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackurlife::Application.config.secret_key_base = 'd65b9f8bf6fcb35541cbc3529a13567df2aeea4ae5c46058e0a4d701ed993d4f6971f57676cd154ff67034c5bf0e48233f11a695056dc818628ca1b7be6109c8'
